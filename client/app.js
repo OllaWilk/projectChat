431c39a856b8e534ed54a0e4c93d414bb59a28fd
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-22 18:19:16 +0200

    HTML references

---
+++ client/app.js
@@ -0,0 +1,18 @@
+'use strict';
+{
+    const opts = {
+        loginForm: '#welcome-form',
+        messagesSection: '#messages-section',
+        messagesList: '#messages-list',
+        addMessageForm: '#add-messages-form',
+        userNameImput: '#username',
+        messageContentInput: '#message-content',
+    };
+
+    const loginForm = document.querySelector(opts.loginForm);
+    const messagesSection = document.querySelector(opts.messagesSection);
+    const messagesList = document.querySelector(opts.messagesList);
+    const addMessageForm = document.querySelector(opts.addMessageForm);
+    const userNameImput = document.querySelector(opts.userNameImput);
+    const messageContentInput = document.querySelector(opts.messageContentInput);
+}
